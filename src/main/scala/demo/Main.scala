173548fa5bf7b6c4733d8ee4fc5b8d4a69ec251c
yoninachmany <yoninachmany@gmail.com>
2017-07-10 12:29:34 -0400

    no cl args, hardcode proper args, get error about decodeJpeg and unexpected format

--- src/main/scala/demo/Main.scala
+++ src/main/scala/demo/Main.scala
@@ -2,11 +2,13 @@ package demo
 
 import geotrellis.raster._
 import geotrellis.spark._
+import LabelImage._
 
 object Main {
   def helloSentence = "Hello GeoTrellis"
 
   def main(args: Array[String]): Unit = {
-    println(helloSentence)
+    // println(helloSentence)
+    LabelImage.main(Array("inception5h", "data/013022223121.tif"))
   }
 }
