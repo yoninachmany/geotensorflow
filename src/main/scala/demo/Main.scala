09c620801b25111b92602674476d4b010fda8dde
yoninachmany <yoninachmany@gmail.com>
2017-07-10 17:07:25 -0400

    more test files

--- src/main/scala/demo/Main.scala
+++ src/main/scala/demo/Main.scala
@@ -8,7 +8,9 @@ object Main {
   def helloSentence = "Hello GeoTrellis"
 
   def main(args: Array[String]): Unit = {
-    // println(helloSentence)
-    LabelImage.main(Array("inception5h", "data/013022223121.tif"))
+    LabelImage.main(Array("inception5h", "data/3band_AOI_1_RIO_img1002.tif"))
+    LabelImage.main(Array("inception5h", "data/3band_AOI_1_RIO_img1051.tif"))
+    LabelImage.main(Array("inception5h", "data/3band_AOI_1_RIO_img1078.tif"))
+    LabelImage.main(Array("inception5h", "data/3band_AOI_1_RIO_img1088.tif"))
   }
 }
