059d964ef12b879c66fb6859f3550e7957c32829
yoninachmany <yoninachmany@gmail.com>
2017-07-19 13:15:52 -0400

    inception download in right directory

--- inception5h/download.sh
+++ inception5h/download.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip > inception5h.zip &&
-unzip inception5h.zip && rm inception5h.zip
+curl https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip > inception5h/inception5h.zip &&
+unzip inception5h/inception5h.zip -d inception5h && rm inception5h/inception5h.zip
