bfb19e12b3716d85a5f1fb0d90886ba32bbd8268
yoninachmany <yoninachmany@gmail.com>
2017-07-20 13:15:27 -0400

    scalastyle

--- src/main/scala/demo/GraphBuilder.scala
+++ src/main/scala/demo/GraphBuilder.scala
@@ -1,18 +1,18 @@
-/* Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License")
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package demo
 
 import geotrellis.raster.{Tile, MultibandTile}
@@ -21,65 +21,52 @@ import geotrellis.raster.io.geotiff.reader.GeoTiffReader
 import org.tensorflow.{DataType, Graph, Output, Tensor}
 
 import java.nio.ByteBuffer
-import java.awt.image._
+import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
-import ImageIOMultibandTile._
+import ImageIOMultibandTile.convertToMultibandTile
 
 // In the fullness of time, equivalents of the methods of this class should be auto-generated from
 // the OpDefs linked into libtensorflow_jni.so. That would match what is done in other languages
 // like Python, C++ and Go.
+// https://github.com/tensorflow/tensorflow/blob/r1.2/tensorflow/java/src/main/java/org/tensorflow/examples/LabelImage.java#L156-L207
 class GraphBuilder(g: Graph) {
-  def div(x: Output, y: Output): Output = {
-    return binaryOp("Div", x, y)
-  }
-
-  def sub(x: Output, y: Output): Output = {
-    return binaryOp("Sub", x, y)
-  }
-
-  def resizeBilinear(images: Output, size: Output): Output = {
-    return binaryOp("ResizeBilinear", images, size)
-  }
-
-  def expandDims(input: Output, dim: Output): Output = {
-    return binaryOp("ExpandDims", input, dim)
-  }
+  def div(x: Output, y: Output): Output = binaryOp("Div", x, y)
+  def sub(x: Output, y: Output): Output = binaryOp("Sub", x, y)
+  def resizeBilinear(images: Output, size: Output): Output = binaryOp("ResizeBilinear", images, size)
+  def expandDims(input: Output, dim: Output): Output = binaryOp("ExpandDims", input, dim)
 
   def cast(value: Output, dtype: DataType): Output = {
-    return g.opBuilder("Cast", "Cast").addInput(value).setAttr("DstT", dtype).build.output(0)
+    g.opBuilder("Cast", "Cast").addInput(value).setAttr("DstT", dtype).build.output(0)
   }
 
   def decodeJpeg(contents: Output, channels: Long): Output = {
-    return g.opBuilder("DecodeJpeg", "DecodeJpeg")
-        .addInput(contents)
-        .setAttr("channels", channels)
-        .build
-        .output(0);
+    g.opBuilder("DecodeJpeg", "DecodeJpeg").addInput(contents).setAttr("channels", channels).build.output(0);
   }
 
   def constant(name: String, value: Any): Output = {
     val t: Tensor = Tensor.create(value)
-    val o: Output = g.opBuilder("Const", name)
-      .setAttr("dtype", t.dataType)
-      .setAttr("value", t)
-      .build
-      .output(0)
+    val o: Output = g.opBuilder("Const", name).setAttr("dtype", t.dataType).setAttr("value", t).build.output(0)
     t.close
-    return o
+    o
   }
 
   private def binaryOp = true
-  def binaryOp(ty: String, in1: Output, in2: Output): Output = {
-    return g.opBuilder(ty, ty).addInput(in1).addInput(in2).build.output(0)
+  def binaryOp(ty: String, in1: Output, in2: Output): Output = g.opBuilder(ty, ty).addInput(in1).addInput(in2).build.output(0)
+
+  // Added
+  def constantTensor(name: String, t: Tensor): Output = g.opBuilder("Const", name).setAttr("dtype", t.dataType).setAttr("value", t).build.output(0)
+
+  private def isJpg = true
+  def isJpg(imagePathString: String): Boolean = imagePathString.indexOfSlice("jpg") != -1 || imagePathString.indexOfSlice("jpg") != -1
+
+  private def getMultibandTileFromJpg = true
+  def getMultibandTileFromJpg(imagePathString: String): MultibandTile = {
+    val image: BufferedImage = ImageIO.read(new java.io.File(imagePathString));
+    ImageIOMultibandTile.convertToMultibandTile(image)
   }
 
-  def constantTensor(name: String, t: Tensor): Output = {
-    return g.opBuilder("Const", name)
-      .setAttr("dtype", t.dataType)
-      .setAttr("value", t)
-      .build
-      .output(0)
-  }
+  private def getMultibandTileFromTif = true
+  def getMultibandTileFromTif(imagePathString: String): MultibandTile = GeoTiffReader.readMultiband(imagePathString).tile
 
   /**
    * Decode a TIFF-encoded image to a uint8 tensor using GeoTrellis.
@@ -88,16 +75,7 @@ class GraphBuilder(g: Graph) {
    */
   def decodeWithMultibandTile(imagePathString: String): Tensor = {
     // Read GeoTiff: https://geotrellis.readthedocs.io/en/latest/tutorials/reading-geoTiffs.html
-    var tile: MultibandTile = null;
-    val isJpg = imagePathString.indexOfSlice("jpg") != -1 || imagePathString.indexOfSlice("jpg") != -1
-    println(isJpg)
-
-    if (isJpg) {
-      val image: BufferedImage = ImageIO.read(new java.io.File(imagePathString));
-      tile = ImageIOMultibandTile.convertToMultibandTile(image)
-    } else {
-      tile = GeoTiffReader.readMultiband(imagePathString).tile
-    }
+    var tile: MultibandTile = if (isJpg(imagePathString)) then getMultibandTileFromJpg(imagePathString) else getMultibandTileFromTif(imagePathString)
 
     val dataType: DataType = DataType.UINT8
     val height: Int = tile.rows
@@ -120,6 +98,6 @@ class GraphBuilder(g: Graph) {
 
     val data: ByteBuffer = ByteBuffer.wrap(byteArray)
     val imageTensor: Tensor = Tensor.create(dataType, shape, data)
-    return imageTensor
+    imageTensor
   }
 }
